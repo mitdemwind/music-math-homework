40ee37bfb049a8c81220e892f321220038986c97
AlwaySleepy <132333780+AlwaySleepy@users.noreply.github.com>
2023-12-15 23:01:29 +0800

    Update utils.py
    修改了to_pitchclass，把那个改成了12个音符表示
    还添加了一些辅助函数

--- utils.py
+++ utils.py
@@ -2,13 +2,16 @@
 import numpy as np
 from constants import *
 
+
 pitchname = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+
+complete_pitchname = ['C','#C','D','#D','E','F','#F','G','#G','A','#A','B']
 def to_pitchclass(midi_number: int) -> tuple[int]:
     """
     Convert a midi numder into (pitchClass, octaveNumber)
     e.g. to_pitchclass(60) == (0, 4) is the note C4
     """
-    return (MAJSCALE.index(midi_number % 12), midi_number // 12 - 1)
+    return (midi_number % 12, midi_number // 12 - 1)
 
 def to_note(pitchclass: int, octave: int) -> int:
     """
@@ -37,4 +40,15 @@ def to_note_str(note: int) -> str:
         return EXTEND[1]
     pc, octave = to_pitchclass(note)
     return pitchname[pc] + str(octave)
+
+def distance(note1:int,note2:int)->int:
+    """compute the distance(音程) between two notes"""
+    cnt=0
+    name1,level1=to_pitchclass(note1)
+    name2,level2=to_pitchclass(note2)
+    if level1 != level2:
+        cnt+=(level2-level1)*12
+    return abs(cnt+name2-name1)
+
+
 
